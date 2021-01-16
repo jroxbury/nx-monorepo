dd61eac13191dddd36fa5616dde5d395f192d6be
Joe Roxbury <jroxbury@snapfinance.com>
2021-01-16 10:12:06 -0700

    import shared UI card from lib

--- apps/movies/src/app/app.tsx
+++ apps/movies/src/app/app.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import styles from './app.module.scss';
-
+import { Card } from '@monorepo/movies/ui';
 export function App() {
   return (
     <div className={styles.app}>
        Hello, World!
+       <Card/>
     </div>
   );
 }
