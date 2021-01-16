03044658e7aa7018f59a729d2f2201f4967c682b
jroxbury <roxbury.joseph@gmail.com>
2021-01-16 10:57:54 -0700

    add movie interface

--- apps/movies/src/app/app.tsx
+++ apps/movies/src/app/app.tsx
@@ -3,10 +3,18 @@ import React from 'react';
 import styles from './app.module.scss';
 import { Card } from '@monorepo/movies/ui';
 export function App() {
+  const props = {
+        "id":"1",
+        "title":"The Rock",
+        "year":"1996",
+        "genre":"Action/Thriller",
+        "runtime":"2h 16m",
+        "image":"/assets/the-rock.jpeg"
+  };
   return (
     <div className={styles.app}>
        Hello, World!
-       <Card/>
+       <Card {...props}/>
     </div>
   );
 }
---
+++ libs/types/src/lib/movie.interface.ts
@@ -0,0 +1,8 @@
+export interface Movie {
+    id:string;
+    title:string;
+    year:string;
+    genre:string;
+    runtime:string;
+    image:string;
+}
