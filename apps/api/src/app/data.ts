834115204873d63f34bf166f4cfd17172f637051
Joe Roxbury <jroxbury@snapfinance.com>
2021-01-16 10:08:18 -0700

    add some static images

--- apps/api/src/app/data.ts
+++ apps/api/src/app/data.ts
@@ -4,21 +4,24 @@ const data = [
         "title":"The Rock",
         "year":"1996",
         "genre":"Action/Thriller",
-        "runtime":"2h 16m"
+        "runtime":"2h 16m",
+        "image":"/assets/the-rock.jpeg"
     },
     {
         "id":"2",
         "title":"Psycho",
         "year":"1960",
         "genre":"Horror/Thriller",
-        "runtime":"1h 49m"
+        "runtime":"1h 49m",
+        "image":"/assets/psycho.jpg"
     },
     {
         "id":"3",
         "title":"Goodfellas",
         "year":"1990",
         "genre":"Crime/Drama",
-        "runtime":"2h 28m"
+        "runtime":"2h 28m",
+        "image":"/assets/goodfellas.jpg"
     }
 ]
 
