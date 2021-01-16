5893820cf0f73f52bab0305bc4c3b514ae7cb608
Joe Roxbury <jroxbury@snapfinance.com>
2021-01-16 10:09:49 -0700

    generate ui card component

---
+++ libs/movies/ui/src/lib/card/card.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './card.module.scss';
+
+/* eslint-disable-next-line */
+export interface CardProps {}
+
+export function Card(props: CardProps) {
+  return (
+    <div>
+      <h1>Welcome to card!</h1>
+    </div>
+  );
+}
+
+export default Card;
