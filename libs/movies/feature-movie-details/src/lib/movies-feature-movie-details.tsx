1f9b3bee066bd6c3fbced6023ebd70483224421e
jroxbury <roxbury.joseph@gmail.com>
2021-01-20 16:23:26 -0700

    fetch movie by id

--- libs/movies/feature-movie-details/src/lib/movies-feature-movie-details.tsx
+++ libs/movies/feature-movie-details/src/lib/movies-feature-movie-details.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { RouteComponentProps } from 'react-router-dom';
-
+import {Movie} from "@monorepo/types";
 type TParams = { id: string };
 
 import './movies-feature-movie-details.module.scss';
@@ -9,12 +9,19 @@ import './movies-feature-movie-details.module.scss';
 /* eslint-disable-next-line */
 export interface MoviesFeatureMovieDetailsProps extends RouteComponentProps<TParams> {}
 
-export function MoviesFeatureMovieDetails(
-  props: MoviesFeatureMovieDetailsProps
-) {
+export function MoviesFeatureMovieDetails(props: MoviesFeatureMovieDetailsProps) {
+  const [data, setData] = useState<Movie>();
+  useEffect(() => {
+    fetch(`api/movies/${props.match.params.id}`)
+    .then(res => res.json())
+    .then((data:Movie) => {
+      setData(data);
+    })
+  },[props.match.params.id])
+
   return (
     <div>
-      <h1>Welcome to movie {props.match.params.id}!</h1>
+      <h1>{data?.title}</h1>
     </div>
   );
 }
