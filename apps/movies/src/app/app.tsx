8652e6a028c526f807bedc6c5179fff01eec8350
jroxbury <roxbury.joseph@gmail.com>
2021-01-20 15:24:09 -0700

    call api for movies

--- apps/movies/src/app/app.tsx
+++ apps/movies/src/app/app.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import styles from './app.module.scss';
 import { Card } from '@monorepo/movies/ui';
 
-import { Route, Link, useHistory } from 'react-router-dom';
+import { Route, useHistory } from 'react-router-dom';
 import { MoviesFeatureMovieDetails } from '@monorepo/movies/feature-movie-details';
+import { Movie } from '@monorepo/types';
 
 export function App() {
   const props = {
@@ -17,13 +18,50 @@ export function App() {
   };
   const history = useHistory();
 
+  const [state, setState] = useState<{
+    data: Movie[];
+    loadingState: 'success' | 'error' | 'loading';
+  }>({
+    data: [],
+    loadingState: 'success',
+  });
+
+  useEffect(() => {
+    setState({
+      ...state,
+      loadingState: 'loading',
+    });
+    fetch('/api/movies')
+      .then((x) => x.json())
+      .then((res:Movie[]) => {
+        setState({
+          ...state,
+          data: res,
+          loadingState: 'success',
+        });
+      })
+      .catch((err) => {
+        setState({
+          ...state,
+          loadingState: 'error',
+        });
+      });
+  }, []);
+
+
   return (
     <div className={styles.app}>
-      Hello, World!
-      <Card
-        {...props}
-        onClick={() => history.push(`/movie/${props.id}`)}
-      />
+      {state.loadingState === 'loading'
+            ? 'Loading...'
+            : state.loadingState === 'error'
+            ? '<div>Error retrieving data</div>'
+            : state.data.map((props) => (
+              <Card
+                {...props}
+                onClick={() => history.push(`/movie/${props.id}`)}
+              />
+            ))}
+
       <Route
         path="/movie/:id"
         component={MoviesFeatureMovieDetails}
